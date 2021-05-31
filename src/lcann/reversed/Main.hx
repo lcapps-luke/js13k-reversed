96a454710cf99abd5c7b92a92277254fdb0b0865
Luke Cann <luke@lc-apps.co.uk>
2021-05-31 15:36:43 +0100

    Use "requestAnimationFrame" instead of "setInterval" & Improve page scaling

--- src/lcann/reversed/Main.hx
+++ src/lcann/reversed/Main.hx
@@ -1,12 +1,8 @@
 package lcann.reversed;
 
 import js.Browser;
-import js.Lib;
-import js.html.HTMLDocument;
-import js.html.Window;
 import js.html.CanvasElement;
 import js.html.CanvasRenderingContext2D;
-import js.html.CanvasPattern;
 
 /**
  * ...
@@ -18,27 +14,30 @@ class Main {
 	private var canvasElement : CanvasElement;
 	private var context : CanvasRenderingContext2D;
 	private var room : GameRoom;
+	public static var lastFrame:Float = 0;
 	
 	static function main() {
 		keyboard = new Keyboard();
-		
-		var main:Main = new Main();
+		new Main();
 	}
 	
 	public function new() {
-		canvasElement = cast(Browser.document.getElementById("lcann_reversed"));
+		canvasElement = cast(Browser.document.getElementsByTagName("canvas")[0]);
 		context = canvasElement.getContext2d();
 		
 		room = new PlayRoom(canvasElement.width, canvasElement.height);
 		
-		Browser.window.setInterval(onUpdate, 1000 / 60);
+		Browser.window.requestAnimationFrame(onUpdate);
 	}
 	
-	private function onUpdate():Void {
-		var s:Float = 1 / 60;
+	private function onUpdate(thisFrame:Float):Void {
+		var s:Float = (thisFrame - lastFrame) / 1000;
+		lastFrame = thisFrame;
 		
 		context.fillStyle = "#000";
 		context.fillRect(0, 0, canvasElement.width, canvasElement.height);
 		room.update(context, s);
+
+		Browser.window.requestAnimationFrame(onUpdate);
 	}
 }
